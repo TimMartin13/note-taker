38213efb3ad06bdd0b4ab00e5f7eac0a20cfd29e
Mandelbrot <timmartin13@gmail.com>
2021-02-02 22:47:10 -0600

    Finished the post and delete

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -13,49 +14,61 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
+// Sticky Notes (DATA)
 // =============================================================
-// var characters = [
-//   {
-//     routeName: "yoda",
-//     name: "Yoda",
-//     role: "Jedi Master",
-//     age: 900,
-//     forcePoints: 2000
-//   },
-//   {
-//     routeName: "darthmaul",
-//     name: "Darth Maul",
-//     role: "Sith Lord",
-//     age: 200,
-//     forcePoints: 1200
-//   },
-//   {
-//     routeName: "obiwankenobi",
-//     name: "Obi Wan Kenobi",
-//     role: "Jedi Master",
-//     age: 55,
-//     forcePoints: 1350
-//   }
-// ];
+// var notes = [];
 
 // Routes
 // =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-// // Displays all characters
-// app.get("/api/characters", function(req, res) {
-//   return res.json(characters);
-// });
+// Displays all characters
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
 
+// Basic route that sends the user first to the AJAX Page
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.post("/api/notes", function(req, res) {
+    // Get the saved notes
+    let savedData = fs.readFileSync(__dirname + "/db/db.json");
+    // Parse the data
+    let notes = JSON.parse(savedData);
+    // Get the new note from the user
+    let newNote = req.body;
+    // Add a unique ID
+    newNote.id = new Date();
+    // Push it into the current array
+    notes.push(newNote);
+    // Write back to the db.json
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), 'utf-8', (err) => err ? console.error(err) : console.log("Success!"));
+    // Return the new note to the page  
+    res.json(newNote);
+  });
+
+app.delete("/api/notes/:id", function(req, res) {
+    // Get the saved notes
+    let savedData = fs.readFileSync(__dirname + "/db/db.json");
+    // Parse the data
+    let notes = JSON.parse(savedData);
+    // Cycle through the array
+    for (let i = 0; i < notes.length; i++) {
+        // Find the note that matches the ID to be deleted
+        if (req.params.id === notes[i].id) {
+            // Remove the deleted data
+            let deleted = notes.splice(i, 1);
+        }
+    }
+    // Write it back to the file
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), 'utf-8', (err) => err ? console.error(err) : console.log("Success!"));
+    // Send the data back to the user
+    return res.json(notes);
+});
 // // Displays a single character, or returns false
 // app.get("/api/characters/:character", function(req, res) {
 //   var chosen = req.params.character;
